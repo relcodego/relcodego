54ae435501f89ab4b85b26ae2a0c1d0046f6ca20
relcodego <relcodego@outlook.com>
2015-12-27 11:46:42 +0800

    add for c

--- c/add.c
+++ c/add.c
@@ -8,9 +8,12 @@ int main()
 {
 
 	float a,b,c;
+    printf("please input the first number \n");
 	scanf("%f",&a);
 	printf("%f+ \n",a);
+    printf("please input the secound number \n");
 	scanf("%f",&b);
+	printf("%f+%f= \n",a,b);
 	c=a+b;
 	printf("\n %f \n",c);
 
