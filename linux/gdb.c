d4b30331f7ed5f236e1ec8d0822699cb1e1180ad
relcodego <relcodego@outlook.com>
2016-03-29 16:57:07 +0800

    3-29

---
+++ linux/gdb.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc,char *argv[])
+{
+
+    int a,b;
+    int sum;
+    scanf("%d,%d",&a,&b);
+    sum = a+b;
+    printf("sum=%d",sum);
+    return 0;
+}
